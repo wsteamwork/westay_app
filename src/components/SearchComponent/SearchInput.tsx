4c73742d4b1bcba876fad250d43db58273555b52
Đào Duy <duydtd97@gmail.com>
2020-01-13 15:40:28 +0700

    Theme RoomList Screen and Theme Filter Screen

--- src/components/SearchComponent/SearchInput.tsx
+++ src/components/SearchComponent/SearchInput.tsx
@@ -1,208 +1,86 @@
-import React, { FC, useState } from 'react';
-import { Alert, ScrollView, StyleSheet, View, Text } from 'react-native';
-import { SearchBar } from 'react-native-elements';
+import React, {FC} from 'react';
+import {StyleSheet, View} from 'react-native';
+import {SearchBar} from 'react-native-elements';
 import Icon from 'react-native-vector-icons/FontAwesome';
-import { NavigationInjectedProps, withNavigation } from 'react-navigation';
-import {
-  COLOR_BACKGROUND_INPUT,
-  NORMAL,
-  SIZE_TEXT_SUBTITLE,
-  COLOR_TEXT_DEFAULT,
-  COLOR_TEXT_SUBTITLE, SIZE_TEXT_CONTENT, SIZE_TEXT_TITLE, SIZE_TEXT_TITLE_MEDIUM, COLOR_TITLE_HEADER,
-} from 'styles/global.style';
-import { SearchSuggestData } from 'types/Search/SearchResponse';
-import {hp, wp} from 'utils/responsive';
-import CardWithSideText from '../GlobalComponents/CardWithSideText';
-import ListCategory from '../GlobalComponents/ListCategory';
-import TransitionView from '../GlobalComponents/TransitionView';
-import ListPropertySearch from '../ListPropertySearch';
-import { getSuggestion } from './SearchInputContext';
-import {elevationShadowStyle, __currentPlatform} from 'utils/mixins';
+import {NavigationInjectedProps, withNavigation} from 'react-navigation';
+import {NORMAL, SIZE_TEXT_SUBTITLE, COLOR_TEXT_DEFAULT} from 'styles/global.style';
+import {wp} from 'utils/responsive';
+import {elevationShadowStyle} from 'utils/mixins';
 import Ionicons from 'react-native-vector-icons/Ionicons';
-import Feather from 'react-native-vector-icons/Feather';
-import RoomCard from 'components/GlobalComponents/Cards/RoomCard';
 
 interface IProps extends NavigationInjectedProps {
   returnKeyType?: "none" | "default" | "done" | "go" | "next" | "search" | "send" | "previous" | "google" | "join" | "route" | "yahoo" | "emergency-call" | undefined;
   height?: number | string;
-  returnKeyLabel?: string
-};
+  returnKeyLabel?: string;
+  value: string;
+  _onChangeText:(value:string)=>void;
+  _onKeyPress:()=>void;
+}
 
 const GlobalSearchInput: FC<IProps> = (props) => {
-  const { returnKeyType, height, returnKeyLabel, navigation } = props;
-  const [input, setInput] = useState<string>('');
-  const [dataSearchSuggest, setDataSearchSuggest] = useState<Array<SearchSuggestData>>([]);
+  const { returnKeyType, height, returnKeyLabel, value, _onChangeText, _onKeyPress, navigation } = props;
 
-  const _renderItemSearchSuggest = (item: any) => {
-    return (
-      <TransitionView
-        index={dataSearchSuggest.indexOf(item)}
-      >
-        <CardWithSideText
-          hasImage={false}
-          icon={<Ionicons name={'ios-search'} size={18} />}
-          title={item.name}
-          onPress={() => Alert.alert(item.name)}
-          rounded
-        />
-      </TransitionView>
-    );
-  }
-
-  const _onChangeText = (value: any) => {
-    getSuggestion(value).then((res) => {
-      setDataSearchSuggest(res);
-    });
-    setInput(value);
-  }
-
-  const _onKeyPress = () => {
-    // console.log(dataSearchSuggest);
-    setInput(dataSearchSuggest[0].name);
-  }
-
-  const styles = StyleSheet.create({
-    container: {
-      justifyContent:'center',
-      alignItems: 'center',
-      paddingHorizontal: wp('5%'),
-      paddingVertical: wp('2%')
-    },
-    viewInput: {
-      flexDirection: 'row',
-      alignItems: 'center',
-      borderRadius: 50,
-    },
-    textInput: {
-      width: '100%',
-      borderTopWidth: 0,
-      borderBottomWidth: 0,
-      borderColor: 'transparent',
-      padding: 0,
-      margin: 0,
-      borderRadius: 50,
-    },
-    boxInfo:{
-      flexDirection: 'row',
-      marginVertical: hp('2%'),
-      justifyContent: 'space-between'
-    },
-    lineVertical:{
-      width: 1,
-      height: '100%',
-      backgroundColor: COLOR_TEXT_SUBTITLE
-    },
-    boxDate:{
-      width: '50%',
-      justifyContent:'center',
-      alignContent:'center',
-      alignItems:'center',
-    },
-    txtDate:{
-      fontWeight: '700',
-      fontSize: SIZE_TEXT_TITLE_MEDIUM
-    },
-    boxFilter:{
-      flexDirection:'row',
-      justifyContent: 'space-between',
-      alignItems: 'center',
-      padding: wp('2%')
-    }
-  });
   return (
-    <ScrollView stickyHeaderIndices={[0]}>
-      <View style={styles.container}>
-        <View style = {[styles.viewInput, elevationShadowStyle(10)]}>
-          <SearchBar
-            value={input}
-            onChangeText={_onChangeText}
-            onSubmitEditing={() => _onKeyPress()}
-            underlineColorAndroid="transparent"
-            placeholderTextColor={'#7676769d'}
-            placeholder={'Search anything'}
-            clearButtonMode={'always'}
-            showCancel
-            clearIcon={<Icon name="keyboard-o" color={'#7676769d'} />}
-            cancelButtonTitle={'Cancel'}
-            keyboardType={'default'}
-            containerStyle={styles.textInput}
-            returnKeyType={returnKeyType}
-            returnKeyLabel={returnKeyLabel}
-            autoCorrect={false}
-            inputContainerStyle={{
-              // height: height || hp('4.2%'),
-              backgroundColor: 'white',
-              borderRadius: 50
-            }}
-            inputStyle={{
-              color: '#424242',
-              fontSize: SIZE_TEXT_SUBTITLE,
-              fontWeight: NORMAL,
-            }}
-            searchIcon = {
-              <Ionicons
-                name = {'ios-search'}
-                color = {COLOR_TEXT_DEFAULT}
-                size = {wp('5%')}
-                style = {{paddingLeft: wp('3%')}}
-              />
-            }
+    <View style = {[styles.viewInput, elevationShadowStyle(10)]}>
+      <SearchBar
+        value={value}
+        onChangeText={(value)=>_onChangeText(value)}
+        onSubmitEditing={() => _onKeyPress}
+        underlineColorAndroid="transparent"
+        placeholderTextColor={'#7676769d'}
+        placeholder={'Search anything'}
+        clearButtonMode={'always'}
+        showCancel
+        clearIcon={<Icon name="keyboard-o" color={'#7676769d'} />}
+        cancelButtonTitle={'Cancel'}
+        keyboardType={'default'}
+        containerStyle={styles.textInput}
+        returnKeyType={returnKeyType}
+        returnKeyLabel={returnKeyLabel}
+        autoCorrect={false}
+        inputContainerStyle={{
+          // height: height || hp('4.2%'),
+          backgroundColor: 'white',
+          borderRadius: 50
+        }}
+        inputStyle={{
+          color: '#424242',
+          fontSize: SIZE_TEXT_SUBTITLE,
+          fontWeight: NORMAL,
+        }}
+        searchIcon = {
+          <Ionicons
+            name = {'ios-search'}
+            color = {COLOR_TEXT_DEFAULT}
+            size = {wp('5%')}
+            style = {{paddingLeft: wp('3%')}}
           />
-        </View>
-        <View style={styles.boxInfo}>
-          <View style={styles.boxDate}>
-            <Text style={{color: COLOR_TEXT_SUBTITLE, fontSize: SIZE_TEXT_CONTENT}}>Choose Date</Text>
-            <Text style={styles.txtDate}>12 Dec - 22 Dec</Text>
-          </View>
-
-          <View style={styles.lineVertical}/>
-
-          <View style={styles.boxDate}>
-            <Text style={{color: COLOR_TEXT_SUBTITLE, fontSize: SIZE_TEXT_CONTENT}}>Number of Rooms</Text>
-            <Text style={styles.txtDate}>1 Room - 2 Adults</Text>
-          </View>
-        </View>
-        <View>
-          <RoomCard />
-        </View>
-
-        <View style={styles.boxFilter}>
-          <Text>
-            299 rooms found
-          </Text>
-          <View style={{flexDirection:'row', alignItems:'center'}}>
-            <Text>Filter</Text>
-            <Feather
-              name={'filter'}
-              size={wp('3.5%')}
-              style={{marginLeft: wp('2%')}}
-              color={COLOR_TITLE_HEADER}
-            />
-          </View>
-        </View>
-      </View>
-      {
-        input && dataSearchSuggest ? (
-          <View>
-            <ListCategory
-              title={'Search suggestion'}
-              hasDivider
-              renderItem={_renderItemSearchSuggest}
-              data={dataSearchSuggest}
-            />
-          </View>
-        ) : (
-            <View>
-              <ListPropertySearch />
-            </View>
-          )
-      }
-    </ScrollView>
+        }
+      />
+    </View>
   );
 };
 
 GlobalSearchInput.defaultProps = {
   returnKeyType: 'search',
   returnKeyLabel: 'Search'
-}
+};
+
+const styles = StyleSheet.create({
+  viewInput: {
+    flexDirection: 'row',
+    alignItems: 'center',
+    borderRadius: 50,
+  },
+  textInput: {
+    width: '100%',
+    borderTopWidth: 0,
+    borderBottomWidth: 0,
+    borderColor: 'transparent',
+    padding: 0,
+    margin: 0,
+    borderRadius: 50,
+  }
+});
+
 export default withNavigation(GlobalSearchInput);
--- src/components/SearchComponent/index.tsx
+++ src/components/SearchComponent/index.tsx
@@ -1,25 +1,152 @@
-import React, { FC } from 'react';
-import {StyleSheet, View, Text} from 'react-native';
+import React, {FC, useState, useEffect} from 'react';
+import {StyleSheet, View, Text, ScrollView, Alert} from 'react-native';
 import SearchInput from './SearchInput';
 import {wp, hp} from 'utils/responsive';
+import {COLOR_TEXT_SUBTITLE, SIZE_TEXT_TITLE_MEDIUM, SIZE_TEXT_CONTENT, COLOR_TITLE_HEADER} from 'styles/global.style';
+import {elevationShadowStyle} from 'utils/mixins';
+import RoomCard from 'components/GlobalComponents/Cards/RoomCard';
+import Feather from 'react-native-vector-icons/Feather';
+import ListCategory from 'components/GlobalComponents/ListCategory';
+import ListPropertySearch from 'components/ListPropertySearch';
+import TransitionView from 'components/GlobalComponents/TransitionView';
+import CardWithSideText from 'components/GlobalComponents/CardWithSideText';
+import Ionicons from 'react-native-vector-icons/Ionicons';
+import {SearchSuggestData} from 'types/Search/SearchResponse';
+import {getSuggestion} from 'components/SearchComponent/SearchInputContext';
 
 interface IProps { };
 
 const SearchComponent: FC<IProps> = (props) => {
+  const [dataSearchSuggest, setDataSearchSuggest] = useState<Array<SearchSuggestData>>([]);
+  const [input, setInput] = useState<string>('');
+
+  useEffect(() => {
+    getSuggestion('Ha Noi').then((res) => {
+      setDataSearchSuggest(res);
+    });
+  }, [input]);
+
+
+  const _onChangeText = (value: any) => {
+    getSuggestion(value).then((res) => {
+      setDataSearchSuggest(res);
+    });
+    setInput(value);
+  };
+
+  const _onKeyPress = () => {
+    // console.log(dataSearchSuggest);
+    setInput(dataSearchSuggest[0].name);
+  };
+
+  const _renderItemSearchSuggest = (item: any) => {
+    return (
+      <TransitionView
+        index={dataSearchSuggest.indexOf(item)}
+      >
+        <CardWithSideText
+          hasImage={false}
+          icon={<Ionicons name={'ios-search'} size={18} />}
+          title={item.name}
+          onPress={() => Alert.alert(item.name)}
+          rounded
+        />
+      </TransitionView>
+    );
+  }
 
   return (
-    <View style={styles.container}>
-      <SearchInput />
-    </View>
+    <ScrollView stickyHeaderIndices={[0]}>
+      <View style={styles.container}>
+
+          <SearchInput value={input} _onChangeText={(value => _onChangeText(value))} _onKeyPress={()=>_onKeyPress()}/>
+
+          <View style={styles.boxInfo}>
+            <View style={styles.boxDate}>
+              <Text style={{color: COLOR_TEXT_SUBTITLE, fontSize: SIZE_TEXT_CONTENT}}>Choose Date</Text>
+              <Text style={styles.txtDate}>12 Dec - 22 Dec</Text>
+            </View>
+
+            <View style={styles.lineVertical}/>
+
+            <View style={styles.boxDate}>
+              <Text style={{color: COLOR_TEXT_SUBTITLE, fontSize: SIZE_TEXT_CONTENT}}>Number of Rooms</Text>
+              <Text style={styles.txtDate}>1 Room - 2 Adults</Text>
+            </View>
+          </View>
+
+          <View>
+            <RoomCard />
+          </View>
+
+          <View style={styles.boxFilter}>
+            <Text>
+              299 rooms found
+            </Text>
+            <View style={{flexDirection:'row', alignItems:'center'}}>
+              <Text>Filter</Text>
+              <Feather
+                name={'filter'}
+                size={wp('3.5%')}
+                style={{marginLeft: wp('2%')}}
+                color={COLOR_TITLE_HEADER}
+              />
+            </View>
+          </View>
+
+        {
+          input && dataSearchSuggest ? (
+            <View>
+              <ListCategory
+                title={'Search suggestion'}
+                hasDivider
+                renderItem={_renderItemSearchSuggest}
+                data={dataSearchSuggest}
+              />
+            </View>
+          ) : (
+            <View>
+              <ListPropertySearch />
+            </View>
+          )
+        }
+      </View>
+    </ScrollView>
   );
 };
 
 const styles = StyleSheet.create({
-  container:{
-    // flex:1,
-    // backgroundColor:'grey',
-    // width: wp('90%'),
-    // height: hp('20%')
+  container: {
+    justifyContent:'center',
+    alignItems: 'center',
+    paddingHorizontal: wp('5%'),
+    paddingVertical: wp('2%')
+  },
+  boxInfo:{
+    flexDirection: 'row',
+    marginVertical: hp('2%'),
+    justifyContent: 'space-between'
+  },
+  lineVertical:{
+    width: 1,
+    height: '100%',
+    backgroundColor: COLOR_TEXT_SUBTITLE
+  },
+  boxDate:{
+    width: '50%',
+    justifyContent:'center',
+    alignContent:'center',
+    alignItems:'center',
+  },
+  txtDate:{
+    fontWeight: '700',
+    fontSize: SIZE_TEXT_TITLE_MEDIUM
+  },
+  boxFilter:{
+    flexDirection:'row',
+    justifyContent: 'space-between',
+    alignItems: 'center',
+    padding: wp('2%')
   }
 });
 SearchComponent.defaultProps = {
