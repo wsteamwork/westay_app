88f8599528bf678ea340806783f1b21307db7c05
Duc Nhat <phamducnhat96bkhn@gmail.com>
2020-02-03 17:11:41 +0700

    done profile

--- src/navigation/RootNavigation.tsx
+++ src/navigation/RootNavigation.tsx
@@ -14,6 +14,7 @@ import MyMapView from 'modules/MapView';
 import BottomNavigation from './BottomNavigation';
 import EditProfile from 'components/EditProfile';
 import ListRooms from 'components/ListRooms';
+import SearchSuggest from 'components/SearchComponent';
 
 const stackNavigator = createStackNavigator({
   Main: {
@@ -31,7 +32,7 @@ const stackNavigator = createStackNavigator({
   BoxImageLT: { screen: BoxImageLT },
   BoxDetailRoom: { screen: BoxDetailRoom },
   ListRooms: { screen: ListRooms},
-    SearchSuggest: { screen: SearchSuggest },
+  SearchSuggest: { screen: SearchSuggest },
   Filter: { screen: Filter},
   MapFilter: { screen: MyMapView},
 },
