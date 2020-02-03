dd22e8f65ace2c2965e54bdebcbe7788f30a5331
Duc Nhat <phamducnhat96bkhn@gmail.com>
2020-02-03 17:47:57 +0700

    done

--- src/components/LTRoom/BoxDetailRoom/BoxBookingRoom/index.tsx
+++ src/components/LTRoom/BoxDetailRoom/BoxBookingRoom/index.tsx
@@ -5,6 +5,7 @@ import { Text, Image, Avatar } from 'react-native-elements';
 import { useSelector } from 'react-redux';
 import { ReducersList } from 'store/redux/reducers';
 import ButtonOriginal from 'components/Utils/ButtonOriginal';
+import { TouchableOpacity } from 'react-native-gesture-handler';
 
 interface IProps {
   initialProps?: any;
@@ -25,7 +26,9 @@ const BoxBookingRoom: FC<IProps> = (props) => {
           <Text style={{ lineHeight: 37, fontSize: 10 }}> /month</Text>
         </Text>
       </View>
-      <ButtonOriginal title="Book Now" width={wp('40%')} handlePress={() => Alert.alert('book room')}/>
+      <TouchableOpacity style={styles.buttonStyle} onPress={() => Alert.alert('book room')}>
+        <Text style={styles.titleStyle}>Book Now</Text>
+      </TouchableOpacity>
     </View>
   );
 };
@@ -54,6 +57,27 @@ const styles = StyleSheet.create({
     lineHeight: 30,
     fontSize: 24,
     fontWeight: 'bold',
+  },
+  buttonStyle: {
+    width: wp('30%'),
+    height: wp('10%'),
+    borderRadius: 25,
+    backgroundColor: 'rgb(84, 211, 194)',
+    justifyContent: 'center',
+    alignItems: 'center',
+    shadowColor: '#000',
+    shadowOffset: {
+      width: 0,
+      height: 2,
+    },
+    shadowOpacity: 0.25,
+    shadowRadius: 3.84,
+    elevation: 5,
+  },
+  titleStyle: {
+    color: 'white',
+    fontWeight: '500',
+    marginRight: 3,
   },
 });
 BoxBookingRoom.defaultProps = {};
