22bd6c59b5ae3edb239945b4291b33637a88ef70
Đào Duy <duydtd97@gmail.com>
2020-02-16 23:51:59 +0700

    Merge đặt phòng của Nhất và Splah intro của Duy

--- src/components/LTRoom/BoxDetailRoom/BoxImageLT/index.tsx
+++ src/components/LTRoom/BoxDetailRoom/BoxImageLT/index.tsx
@@ -1,61 +1,93 @@
-import React, {FC, useEffect} from 'react';
-import {StyleSheet, View, ImageBackground, TouchableOpacity, StatusBar} from 'react-native';
+import React, {FC, useEffect, Fragment, useMemo, useState} from 'react';
+import {StyleSheet, View, ImageBackground, TouchableOpacity, StatusBar, Alert, ActivityIndicator} from 'react-native';
 import {wp, hp} from 'utils/responsive';
 import BoxIntroRoom from './BoxIntroRoom';
 import AntDesign from 'react-native-vector-icons/AntDesign';
 import {NavigationInjectedProps, withNavigation} from 'react-navigation';
 import {useSelector, useDispatch} from 'react-redux';
 import {ReducersList} from 'store/redux/reducers';
-import {IMAGE_STORAGE_LG, IMAGE_NOT_FOUND} from 'types/globalTypes';
+import {IMAGE_STORAGE_LG, IMAGE_NOT_FOUND, IMAGE_STORAGE_SM} from 'types/globalTypes';
 import {Dispatch} from 'redux';
 import {handleShareSocial} from 'utils/mixins';
 import {useTranslation} from 'react-i18next';
-
 import {LTRoomReducerAction, getDataLTRoom} from 'store/redux/reducers/LTRoom/RoomDetails';
+import {LTRoomIndexRes} from 'types/LTR/LTRoom/LTRoom';
+import Loadable from 'react-loadable';
+
+const Loading = Loadable({
+  loader: () => import('components/GlobalComponents/LoadingScreen'),
+  loading: () => null,
+});
 
 interface IProps extends NavigationInjectedProps {
 }
 
 const BoxImageLT: FC<IProps> = (props) => {
-  const { navigation } = props;
-  const dispatch = useDispatch<Dispatch<LTRoomReducerAction>>();
-  const listing = useSelector<ReducersList, any>((state) => state.ltRoomDetails.room);
-  const idRoom = navigation.getParam('idRoom');
-  const { t } = useTranslation();
+  const {navigation}          = props;
+  const dispatch              = useDispatch<Dispatch<LTRoomReducerAction>>();
+  const listing               = useSelector<ReducersList, LTRoomIndexRes | null>((state) => state.ltRoomDetails.room);
+  const idRoom                = navigation.getParam('idRoom');
+  const {t}                   = useTranslation();
+  const [loading, setLoading] = useState(false);
 
+  const getResponse = async () => {
+    setLoading(true);
+
+    await getDataLTRoom(idRoom, dispatch).then(() => {
+        setLoading(false);
+      },
+    ).catch(err => {
+      setLoading(false);
+      Alert.alert("Error", "" + err)
+    })
+  };
 
   useEffect(() => {
-    getDataLTRoom(idRoom, dispatch);
+    getResponse();
   }, [idRoom]);
 
   return (
-    <View style={styles.container} collapsable={false}>
-      <View style={styles.featureImage}>
-        <View style={styles.btnBack}>
-          <TouchableOpacity style={styles.bgIcon} onPress={() => navigation.goBack()}>
-            <AntDesign name="arrowleft" size={24} color="#fff" />
-          </TouchableOpacity>
-        </View>
-        <View>
-          <TouchableOpacity style={styles.bgIcon}>
-            <AntDesign name="sharealt" size={24} color="#fff" onPress={()=>handleShareSocial('fb', `https://westay.vn/long-term-room/${idRoom}`, t)}/>
-          </TouchableOpacity>
-        </View>
-      </View>
-      <ImageBackground
-        source={{
-          uri: listing.avatar.images && listing.avatar.images.length
-            ? `${IMAGE_STORAGE_LG + listing.avatar.images[0].name}`
-            : IMAGE_NOT_FOUND,
-        }}
-        style={{ width: '100%', height: '100%' }}>
-        <BoxIntroRoom />
-      </ImageBackground>
-    </View>
+    <Fragment>
+      {useMemo(
+        () => (
+          <Loading loading = {loading} />
+        ),
+        [loading],
+      )}
+      {useMemo(
+        () =>
+          listing && (
+            <View style = {styles.container} collapsable = {false}>
+              <View style = {styles.featureImage}>
+                <View style = {styles.btnBack}>
+                  <TouchableOpacity style = {styles.bgIcon} onPress = {() => navigation.goBack()}>
+                    <AntDesign name = 'arrowleft' size = {24} color = '#fff' />
+                  </TouchableOpacity>
+                </View>
+                <View>
+                  <TouchableOpacity style = {styles.bgIcon}>
+                    <AntDesign name = 'sharealt' size = {24} color = '#fff'
+                               onPress = {() => handleShareSocial('fb', `https://westay.vn/long-term-room/${idRoom}`, t)} />
+                  </TouchableOpacity>
+                </View>
+              </View>
+              <ImageBackground
+                source = {{
+                  uri: listing.avatar.images && listing.avatar.images.length
+                    ? `${IMAGE_STORAGE_SM + listing.avatar.images[0].name}`
+                    : IMAGE_NOT_FOUND,
+                }}
+                style = {{width: '100%', height: '100%'}}
+              >
+                <BoxIntroRoom />
+              </ImageBackground>
+            </View>
+          ), [listing, idRoom])}
+    </Fragment>
   );
 };
 
-const styles = StyleSheet.create({
+const styles            = StyleSheet.create({
   container: {
     flex: 1,
     flexDirection: 'row',
@@ -76,7 +108,7 @@ const styles = StyleSheet.create({
     paddingHorizontal: wp('5%'),
   },
   btnBack: {
-    flex: 1
+    flex: 1,
   },
   bgIcon: {
     width: 40,
